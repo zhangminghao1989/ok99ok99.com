f40c6a06390f264d80217d620b4b2e2db0fb3367
张明浩 <tankririri@users.noreply.github.com>
2023-03-21 15:51:54 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         学达云教育-建造师继续教育-自动上课
 // @namespace    https://github.com/tankririri/ok99ok99.com
-// @version      1.7.5
+// @version      1.7.6
 // @description  自动提交和播放下一节
 // @license      GNU GPLv3
 // @match        https://*.ok99ok99.com/*
@@ -15,21 +15,25 @@ console.log("学达云教育-建造师继续教育-自动上课开始运行！")
 		var info = 0;
 		var myval = setInterval(function () {
 			if(!/p[0-9]+\.ok99ok99\.com/.test(window.location.host)){
-				if($("#divwxck > div").length == 0){
+				if($("#divwxck > div").length == 0 && $("#divwxck").length == 1){
 					alert("请进行微信验证，验证完成后手动刷新页面！");
 					console.log("请进行微信验证，验证完成后手动刷新页面！");
 					clearInterval(myval);
 				} else {
-					if(info == 0){
-						$("p:contains('学习时间大于计划时间才能提交')").after("<b>手动播放视频后开始自动上课！</b>");
-						info = 1;
-					}
-					if($(".submit_btn").attr("disabled") != "disabled"){
-						$(".submit_btn").click();
-					}
-					if($("span i.ico_shipin2.color_blue").length == 0){
-						$(".ico_shipin2:first").click();
-					}
+					if($("#divwxck").length == 1){
+						if(info == 0){
+							$("p:contains('学习时间大于计划时间才能提交')").after("<b>手动播放视频后开始自动上课！</b>");
+							info = 1;
+						}
+						if($(".submit_btn").attr("disabled") != "disabled"){
+							$(".submit_btn").click();
+						}
+						if($("span i.ico_shipin2.color_blue").length == 0){
+							$(".ico_shipin2:first").click();
+						}
+					} else {
+						clearInterval(myval);
+						}
 				}
 			}
 		},5000)
