59e72519998154f6748d75c3e4a45b9d5250cb38
张明浩 <tankririri@users.noreply.github.com>
2023-03-22 09:13:28 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         学达云教育-建造师继续教育-自动上课
 // @namespace    https://github.com/tankririri/ok99ok99.com
-// @version      1.8
+// @version      1.8.2
 // @description  自动提交和播放下一节
 // @license      GNU GPLv3
 // @match        https://*.ok99ok99.com/*
@@ -13,12 +13,17 @@ console.log("学达云教育-建造师继续教育-自动上课开始运行！")
 	'use strict';
 	setTimeout(function(){
 		var info = 0;
+		var alert_count = 0;
 		var myval = setInterval(function () {
 			if(!/p[0-9]+\.ok99ok99\.com/.test(window.location.host)){
 				if($("#divwxck > div").length == 0 && $("#divwxck").length == 1){
-					alert("请进行微信验证，验证完成后手动刷新页面！");
-					console.log("请进行微信验证，验证完成后手动刷新页面！");
-					clearInterval(myval);
+					if (alert_count == 0) {
+						alert_count = 1;
+						alert("请进行微信验证，验证完成后手动刷新页面！");
+						console.log("请进行微信验证，验证完成后手动刷新页面！");
+					} else {
+						clearInterval(myval);
+					}
 				} else {
 					if($("#divwxck").length == 1){
 						if(info == 0){
